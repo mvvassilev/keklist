b5bf27d55e253fcb2840dc8cf187366120f71d92
mvvassilev <m.v.vassilev@abv.bg>
2019-11-11 13:57:37 +0800

    Search bar

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from requests.compat import quote_plus
 import requests
 
 from bs4 import BeautifulSoup
+
+BASE_URL = 'https://berlin.craigslist.org/search/hhh?query={}'
 
 def home(request):
     return render(request, 'base.html')
@@ -9,5 +12,8 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
+    response = requests.get('https://berlin.craigslist.org/search/sss?query=car')
+    data = response.text
+    print(data)
     context = {'search': search}
     return render(request, 'myapp/new_search.html', context)
